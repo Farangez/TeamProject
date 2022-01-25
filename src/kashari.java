2f03807275bb6e49c75503af38e765102f596cb3
ozturkkerem <ozturk.kerem@live.com>
2022-01-24 20:55:24 -0500

    Selam

--- src/kashari.java
+++ src/kashari.java
@@ -1,5 +1,11 @@
 public class kashari {
     public static void main(String[] args) {
         System.out.println("Kashari");
+
+
+
+        // Today I will eat Turkish IceCream
+
+        // TOmorrow I will have omlet
     }
 }
