173c84fe72ec7dba68d4f6d5ab72c38051ef419e
cross <crosstown11223@yahoo.com>
2022-01-25 12:47:16 -0500

    added code, how to display triangle

--- src/elenaBaidalo/HelloEveryOne.java
+++ src/elenaBaidalo/HelloEveryOne.java
@@ -2,6 +2,14 @@ package elenaBaidalo;
 
 public class HelloEveryOne {
     public static void main(String[] args) {
+        System.out.println("t\t\t    ^");
+        System.out.println("t\t\t   /  \\");
+        System.out.println("t\t\t  /    \\");
+        System.out.println("t\t\t /      \\");
+        System.out.println("t\t\t/        \\");
+        System.out.println("t\t\t_________");
+
+
 
     }
 
